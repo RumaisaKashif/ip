3b387aa2ee61831f2eead8293ef444698d10b528
Rumaisa Kashif <rumaisa.kashif2109@gmail.com>
2026-01-22 22:06:09 +0800

    Add support for echo

--- src/main/java/Pook.java
+++ src/main/java/Pook.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Pook {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("----------------------");
         System.out.println("Hello! I'm Pook");
         System.out.println("What can I do for you?");
         System.out.println("----------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            System.out.println();
+            String user_input = input.nextLine();
+            System.out.println("----------------------");
+            if (user_input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println();
+                break;
+            }
+            System.out.println(user_input);
+            System.out.println("----------------------");
+            System.out.println();
+        }
+        input.close();
         System.out.println("----------------------");
     }
 }
