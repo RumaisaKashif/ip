d80ea7e4243884b1274e6b0713b181d6eb933cf8
Rumaisa Kashif <rumaisa.kashif2109@gmail.com>
2026-02-16 18:06:40 +0800

    Refactor Parser

--- src/main/java/pook/Parser.java
+++ src/main/java/pook/Parser.java
@@ -25,97 +25,21 @@ public class Parser {
             if (userInput.equals("bye")) {
                 return new Pair<Boolean, String>(false, "Bye. Hope to see you again soon!"); 
             } else if (userInput.startsWith("mark")) {
-                String[] segments = userInput.split(" ");
-                if (segments.length < 2) {
-                    throw new PookException("Please specify which list item needs to be marked.");
-                }
-
-                int taskIndex = Integer.parseInt(segments[1]) - 1;
-                Task markableTask = tasks.getTask(taskIndex);
-
-                if (markableTask.getCompletionStatus()) {
-                    throw new PookException("I've already marked this task as done!");
-                }
-
-                markableTask.setStatus(true);
-                storage.saveFile(tasks.getList());
-                result =  "Nice! I've marked this task as done:\n " + markableTask;
+                result =  parseMarkableTask(userInput, tasks, storage);
             } else if (userInput.startsWith("unmark")) {
-                String[] segments = userInput.split(" ");
-                if (segments.length < 2) {
-                    throw new PookException("Please specify which list item needs to be unmarked.");
-                }
-
-                int taskIndex = Integer.parseInt(segments[1]) - 1;
-                Task unmarkableTask = tasks.getTask(taskIndex);
-
-                if (!unmarkableTask.getCompletionStatus()) {
-                    throw new PookException("I've already unmarked this task!");
-                }
-
-                unmarkableTask.setStatus(false);
-                storage.saveFile(tasks.getList());
-                result = "OK, I've marked this task as not done yet:\n " + unmarkableTask;
+                result =  parseUnmarkableTask(userInput, tasks, storage);
             } else if (userInput.startsWith("find")) {
-                String[] segments = userInput.split(" ");
-                if (segments.length < 2) {
-                    throw new PookException("Please specify the keyword you're looking for.");
-                }
-
-                String keyword = segments[1];
-                TaskList filteredTasks = tasks.filterTasks(keyword);
-                result = filteredTasks.getTaskMatches();
+                result = parseFilterableTasks(userInput, tasks);
             } else if (userInput.startsWith("delete")) {
-                String[] segments = userInput.split(" ");
-                if (segments.length < 2) {
-                    throw new PookException("Please specify which list item needs to be deleted.");
-                }
-
-                int taskIndex = Integer.parseInt(segments[1]) - 1;
-                if (taskIndex >= tasks.getList().size() || taskIndex < 0) {
-                    throw new PookException("Invalid task number!");
-                }
-                Task deletableTask = tasks.remove(taskIndex);
-                storage.saveFile(tasks.getList());
-                result = "Noted. I've removed this task:\n " + deletableTask
-                        + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+                result = parseRemovableTasks(userInput, tasks, storage);
             } else if (userInput.startsWith("todo")) {
-                String[] segments = userInput.split(" ");
-                if (segments.length < 2 || segments[1].trim().isEmpty()) {
-                    throw new PookException("Please specify a description for this todo task.");
-                }
-                Task task = new Todo(segments[1]);
-                tasks.add(task);
-                storage.saveFile(tasks.getList());
-                result = "Got it. I've added this task:\n " + task
-                        + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+                result = parseTodo(userInput, tasks, storage);
             } else if (userInput.startsWith("deadline")) {
-                String[] segments = userInput.substring(9).split(" /by ");
-                if (segments.length < 2 || segments[0].trim().isEmpty() || segments[1].trim().isEmpty()) {
-                    throw new PookException("Please specify a description and due date for this deadline.");
-                }
-                Task task = new Deadline(segments[0], segments[1]);
-                tasks.add(task);
-                storage.saveFile(tasks.getList());
-                result = "Got it. I've added this task:\n " + task
-                        + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+                result = parseDeadline(userInput, tasks, storage);
             } else if (userInput.startsWith("event")) {
-                if (userInput.length() < 7) { 
-                    throw new PookException("Please specify a description, start time, and end time for this event.");
-                }
-                String[] segments = userInput.substring(6).split(" /from | /to ");
-                if (segments.length < 3 || segments[0].trim().isEmpty() || segments[1].trim().isEmpty() || segments[2].trim().isEmpty()) {
-                    throw new PookException("Please specify a desription, start time, and end time for this event.");
-                }
-                Task task = new Event(segments[0], segments[1], segments[2]);
-                tasks.add(task);
-                storage.saveFile(tasks.getList());
-                result = "Got it. I've added this task:\n " + task
-                        + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+                result = parseEvent(userInput, tasks, storage);
             } else if (userInput.equals("list")) {
                 result = tasks.getTaskList();
-            } else if (userInput.isEmpty()) {
-                throw new PookException("Please enter a task.");
             } else {
                 throw new PookException("I didn't get that, please reenter a valid command.");
             }
@@ -124,5 +48,127 @@ public class Parser {
         }
         
         return new Pair<Boolean,String>(true, result);
+    }
+
+    private static String parseMarkableTask(String userInput, TaskList tasks, Storage storage) throws PookException{ 
+        String[] segments = userInput.split(" ");
+
+        if (segments.length < 2) {
+            throw new PookException("Please specify which list item needs to be marked.");
+        }
+
+        int taskIndex = Integer.parseInt(segments[1]) - 1;
+        Task markableTask = tasks.getTask(taskIndex);
+
+        if (markableTask.getCompletionStatus()) {
+            throw new PookException("I've already marked this task as done!");
+        }
+
+        markableTask.setStatus(true);
+        storage.saveFile(tasks.getList());
+
+        return  "Nice! I've marked this task as done:\n " + markableTask;
+    }
+
+    private static String parseUnmarkableTask(String userInput, TaskList tasks, Storage storage) throws PookException{ 
+        String[] segments = userInput.split(" ");
+
+        if (segments.length < 2) {
+            throw new PookException("Please specify which list item needs to be unmarked.");
+        }
+
+        int taskIndex = Integer.parseInt(segments[1]) - 1;
+        Task unmarkableTask = tasks.getTask(taskIndex);
+
+        if (!unmarkableTask.getCompletionStatus()) {
+            throw new PookException("I've already unmarked this task!");
+        }
+
+        unmarkableTask.setStatus(false);
+        storage.saveFile(tasks.getList());
+
+        return "OK, I've marked this task as not done yet:\n " + unmarkableTask;
+    }
+
+    private static String parseFilterableTasks(String userInput, TaskList tasks) throws PookException {
+        String[] segments = userInput.split(" ");
+
+        if (segments.length < 2) {
+            throw new PookException("Please specify the keyword you're looking for.");
+        }
+
+        String keyword = segments[1];
+        TaskList filteredTasks = tasks.filterTasks(keyword);
+
+        return filteredTasks.getTaskMatches();
+    }
+
+    private static String parseRemovableTasks(String userInput, TaskList tasks, Storage storage) throws PookException {
+        String[] segments = userInput.split(" ");
+
+        if (segments.length < 2) {
+            throw new PookException("Please specify which list item needs to be deleted.");
+        }
+
+        int taskIndex = Integer.parseInt(segments[1]) - 1;
+
+        if (taskIndex >= tasks.getList().size() || taskIndex < 0) {
+            throw new PookException("Invalid task number!");
+        }
+
+        Task deletableTask = tasks.remove(taskIndex);
+        storage.saveFile(tasks.getList());
+
+        return "Noted. I've removed this task:\n " + deletableTask
+                + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+    }
+
+    private static String parseTodo(String userInput, TaskList tasks, Storage storage) throws PookException {
+        String[] segments = userInput.split(" ");
+
+        if (segments.length < 2 || segments[1].trim().isEmpty()) {
+            throw new PookException("Please specify a description for this todo task.");
+        }
+
+        Task task = new Todo(segments[1]);
+        tasks.add(task);
+        storage.saveFile(tasks.getList());
+
+        return "Got it. I've added this task:\n " + task
+                + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+    }
+
+    private static String parseDeadline(String userInput, TaskList tasks, Storage storage) throws PookException {
+        String[] segments = userInput.substring(9).split(" /by ");
+
+        if (segments.length < 2 || segments[0].trim().isEmpty() || segments[1].trim().isEmpty()) {
+            throw new PookException("Please specify a description and due date for this deadline.");
+        }
+
+        Task task = new Deadline(segments[0], segments[1]);
+        tasks.add(task);
+        storage.saveFile(tasks.getList());
+
+        return "Got it. I've added this task:\n " + task
+                + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
+    }
+
+    private static String parseEvent(String userInput, TaskList tasks, Storage storage) throws PookException{ 
+        if (userInput.length() < 7) { 
+            throw new PookException("Please specify a description, start time, and end time for this event.");
+        }
+
+        String[] segments = userInput.substring(6).split(" /from | /to ");
+
+        if (segments.length < 3 || segments[0].trim().isEmpty() || segments[1].trim().isEmpty() || segments[2].trim().isEmpty()) {
+            throw new PookException("Please specify a desription, start time, and end time for this event.");
+        }
+
+        Task task = new Event(segments[0], segments[1], segments[2]);
+        tasks.add(task);
+        storage.saveFile(tasks.getList());
+
+        return "Got it. I've added this task:\n " + task
+                + "\nNow you have " + tasks.getList().size() + " tasks in the list.";
     }
 }
