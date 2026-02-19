d29c065e84d7cb9bba857620786cc09b8cb40bf5
Rumaisa Kashif <rumaisa.kashif2109@gmail.com>
2026-02-19 07:19:17 +0500

    Resolve merge conflicts

--- src/main/java/pook/Todo.java
+++ src/main/java/pook/Todo.java
@@ -14,7 +14,7 @@ public class Todo extends Task {
 
     @Override
     public String toStorableString() { 
-        return "T | " + getStatusNumber() + " | " + taskDescription;
+        return "T | " + getStatusNumber() + " | " + description;
     }
 
     @Override
