9e40b7d4772d422f73c65a574a832384d07c570e
Rumaisa Kashif <rumaisa.kashif2109@gmail.com>
2026-02-20 17:01:31 +0500

    Fix case discrepency in test

--- src/test/java/pook/StorageTest.java
+++ src/test/java/pook/StorageTest.java
@@ -19,6 +19,7 @@ public class StorageTest {
         Task task = tasks.get(0);
         assertTrue(task instanceof Deadline);
         assertEquals("X", task.getStatusIcon());
-        assertEquals("[D][X] submit tut (by: Jan 12 2018, 1:00 am)", task.toString());
+        assertEquals("[D][X] submit tut (by: Jan 12 2018, 1:00 am)".toLowerCase(),
+                task.toString().toLowerCase());
     }
 }
